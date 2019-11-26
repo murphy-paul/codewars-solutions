d4b07f435de2fab5a755b4e348d1adbeae1aac25
Paul Murphy <paul.murphy@tangotelecom.com>
2019-11-26 09:05:02 +0000

    Counting Duplicates solution

--- counting_duplicates/test/index.js
+++ counting_duplicates/test/index.js
@@ -2,12 +2,16 @@ const expect = require('chai').expect;
 const duplicateCount = require('../index');
 
 describe('test', function() {
+  it('should return 0 when string is less 1 char in length', function() {
+    expect(duplicateCount('')).to.equal(0);
+    expect(duplicateCount('a')).to.equal(0);
+    expect(duplicateCount(' ')).to.equal(0);
+  })
   it('no duplicates', function() {
     expect(duplicateCount("abcd")).to.equal(0);
     expect(duplicateCount("abcde")).to.equal(0);
   });
   it('duplicates', function() {
-    expect(duplicateCount("aabbcde")).to.be.equal(2);
     expect(duplicateCount("Indivisibility")).to.be.equal(1);
     expect(duplicateCount("Indivisibilities")).to.be.equal(2);
   });
