b8eaa8123c68ed113a176f24095e9cae8706e670
Paul Murphy <paul.murphy@tangotelecom.com>
2019-11-26 09:05:02 +0000

    Counting Duplicates solution

--- counting_duplicates/index.js
+++ counting_duplicates/index.js
@@ -1,7 +1,26 @@
 const duplicateCount = (text) => {
-    console.log(text.toLowerCase());
-    
-    return 0;
+    if (text.length <= 1) {
+        return 0;
+    }
+    let arr = text.toLowerCase().split('').sort();
+
+    let map = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        if (map.has(arr[i])) {
+            map.get(arr[i]).count++;
+        } else {
+            map.set(arr[i], {count: 1});
+        }
+    }
+
+    for (let k of map.keys()) {
+        if (map.get(k).count < 2) {
+            map.delete(k);
+        }
+    }
+
+    return map.size;
 };
 
 module.exports = duplicateCount;
